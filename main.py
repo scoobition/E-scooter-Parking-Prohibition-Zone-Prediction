dbd9958d799f7b3553ef8995126957d528b5d499
KwanHo Kwon <kwanoo@syn-172-100-007-026.res.spectrum.com>
2026-01-15 20:50:22 +0900

    임시

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ load_dotenv(BASE_DIR / ".env")
 
 from src.pipeline_geo import geo
 from src.grid import make_predata_and_meta_csv
+from src.viz_grid_map import make_monthly_heatmaps
+from src.result import make_result
 
 if __name__ == "__main__":
     # geo()
-    make_predata_and_meta_csv()
+    # make_predata_and_meta_csv()
+    # make_monthly_heatmaps(opacity=0.5, out_dir="map")
+    make_result()
